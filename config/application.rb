cf7a032aafb764104381c8ecd21975adfdf5d024
dharmjeet <dharmjeet@gmail.com>
2016-09-17 15:50:29 +0530

    secret key base in config file

--- config/application.rb
+++ config/application.rb
@@ -3,8 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'devise'
 
-#this is for secret.yml 
-config.secret_key_base = 'blipblapblup'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
